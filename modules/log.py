87b9fd6b1ed831068436cfc2e8ac60356fd755db
csoeder <charles.soeder@gmail.com>
2015-09-22 23:38:54 -0400

    added a logger.... logging location needs generalized

---
+++ modules/log.py
@@ -0,0 +1,21 @@
+"""
+log.py - Phenny Log Module
+Copyright (c) Esteban Feldman
+
+BSD License
+
+http://github.com/eka/
+"""
+
+from datetime import datetime
+import os
+
+
+def f_log(phenny, input):
+	phial = open('../%s_%s_irc.log'%tuple([datetime.utcnow().month, datetime.utcnow().year]), 'a')
+#	print '%s - %s - %s: %s' % (datetime.utcnow().strftime('%m-%d-%Y %H:%M:%S'), input.sender, input.nick, input)	
+	phial.write( '%s - %s - %s: %s\n' % (datetime.utcnow().strftime('%m-%d-%Y %H:%M:%S'), input.sender, input.nick, input))
+	phial.close()
+f_log.rule = r'(.+)'
+f_log.priority = 'high'
+f_log.thread = False
